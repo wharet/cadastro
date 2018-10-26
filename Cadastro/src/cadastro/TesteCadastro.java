cde29104c194f30351fbce7bef19e94fcd9df571
ads <ads@ads-VirtualBox>
2018-10-25 22:25:36 -0300

    eraeweawe

---
+++ Cadastro/src/cadastro/TesteCadastro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastro;
+
+import java.io.IOException;
+
+public class TesteCadastro {
+    public static void main(String[] args) throws IOException {
+        Cadastro cadastro = new Cadastro();
+        
+        cadastro.CriaMenu();
+    }
+    
+}
